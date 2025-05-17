064ddbe124034e78f40a37b88bd71ba168a19f99
xaivntaajher <dn_her@yahoo.com>
2025-05-16 19:55:36 -0500

    create event handle for onsubmit to update smoothiecard when clicking button

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -4,7 +4,7 @@ const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
-      <h3>{smoothie.method}</h3>
+      <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
       <div className="buttons">
         <Link to={"/" + smoothie.id}>
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -9,6 +9,32 @@ const Update = () => {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [rating, setRating] = useState("");
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !method || !rating) {
+      setFormError("Please fill in all the fields correctly");
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("smoothies")
+      .update({ title, method, rating })
+      .eq("id", id)
+      .select();
+
+    if (error) {
+      console.log(error);
+      setFormError("Please fill in all the fields correctly");
+    }
+    if (data) {
+      console.log(data);
+      setFormError(null);
+      navigate("/");
+    }
+  };
 
   useEffect(() => {
     const fetchSmoothies = async () => {
@@ -33,7 +59,7 @@ const Update = () => {
 
   return (
     <div className="page update">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
         <input
           type="text"
@@ -56,7 +82,7 @@ const Update = () => {
         />
         <button>Update Smoothie Recipe</button>
 
-        {/* {formError && <p className="error">{formError}</p>} */}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
